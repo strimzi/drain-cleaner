775b8ac14bcc6b153e17c74ba6d4038d397874c1
Jakub Scholz <www@scholzj.com>
2021-09-22 17:25:55 +0200

    Add pipeline for rebuild of container images to address CVEs in base images (#20)
    * Add pipeline for rebuilding images in case of base image CVEs
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>
    
    * Add Jakub's email as well
    
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .azure/scripts/container-push.sh
+++ .azure/scripts/container-push.sh
@@ -3,15 +3,10 @@ set -e
 
 echo "Build reason: ${BUILD_REASON}"
 echo "Source branch: ${BRANCH}"
+echo "Pushing container images for ${DOCKER_TAG}"
 
 # Tag and Push the container
 echo "Login into Docker Hub ..."
 docker login -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY
-
-if [ "$BRANCH" == "refs/heads/main" ]; then
-    export DOCKER_TAG="latest"
-else
-    export DOCKER_TAG="${BRANCH#refs/tags/}"
-fi
 
 make docker_load docker_tag docker_push
