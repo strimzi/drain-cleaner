ad8296d6c56a4bf737a122c10c00334668f8ea9d
Paolo Patierno <ppatierno@live.com>
2021-10-25 21:50:25 +0200

    Fixed typo release script
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- .azure/scripts/release-artifacts.sh
+++ .azure/scripts/release-artifacts.sh
@@ -4,6 +4,6 @@ set -e
 echo "Build reason: ${BUILD_REASON}"
 echo "Source branch: ${BRANCH}"
 
-echo "Releasing cartifacts for ${RELEASE_VERSION}"
+echo "Releasing artifacts for ${RELEASE_VERSION}"
 
 make release
