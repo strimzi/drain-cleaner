f4ad142b3064b1b9167cf413cc0f99e06d776a61
Vanessa Busch <vbusch@redhat.com>
2022-06-24 16:28:07 -0400

    Bump quarkus version from 2.5.2 to 2.11.1
    Signed-off-by: Vanessa Busch <vbusch@redhat.com>

--- src/test/java/io/strimzi/utils/k8s/cluster/Kubernetes.java
+++ src/test/java/io/strimzi/utils/k8s/cluster/Kubernetes.java
@@ -5,21 +5,19 @@
 package io.strimzi.utils.k8s.cluster;
 
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.http.HttpClient;
 import io.fabric8.kubernetes.client.utils.HttpClientUtils;
 import io.strimzi.utils.executor.Exec;
 import io.strimzi.utils.k8s.KubeClient;
 import io.strimzi.utils.k8s.cmdClient.KubeCmdClient;
 import io.strimzi.utils.k8s.cmdClient.Kubectl;
 import io.strimzi.utils.k8s.exception.KubeClusterException;
-import okhttp3.OkHttpClient;
-import okhttp3.Protocol;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.time.Duration;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A {@link KubeCluster} implementation for any {@code Kubernetes} cluster.
@@ -53,13 +51,13 @@ public class Kubernetes implements KubeCluster {
 
     @Override
     public KubeClient defaultClient() {
-        OkHttpClient httpClient = HttpClientUtils.createHttpClient(CONFIG);
+        HttpClient httpClient = HttpClientUtils.createHttpClient(CONFIG);
 
         httpClient = httpClient.newBuilder()
-            .protocols(Collections.singletonList(Protocol.HTTP_1_1))
-            .connectTimeout(Duration.ofSeconds(60))
-            .writeTimeout(Duration.ofSeconds(60))
-            .readTimeout(Duration.ofSeconds(60))
+            .preferHttp11()
+            .connectTimeout(60L, TimeUnit.SECONDS)
+            .writeTimeout(60L, TimeUnit.SECONDS)
+            .readTimeout(60L, TimeUnit.SECONDS)
             .build();
 
         return new KubeClient(new DefaultKubernetesClient(httpClient, CONFIG), "default");
